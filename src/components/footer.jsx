b2e32744021489cefa8b5061fa13876de4d129c3
mariosabda <mariosabda@gmail.com>
2024-08-27 19:07:05 +0700

    .

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer class="bg-gray-800 shadow">
             <div class="w-full p-4 gap-y-4 py-4">
             <p class="flex justify-center text-sm text-white text-center">
-            - Proud to Use Indonesia Domain -
+            - Proud to Use 🇮🇩 Domain -
             </p>
             <p class="flex justify-center text-sm text-white text-center">
                 <span className="font-semibold">Copyright@2024</span>. All rights reserved
