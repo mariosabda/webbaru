fb331827e61da8bec4f385cd4359738cf0377dc6
mariosabda <mariosabda@gmail.com>
2024-08-27 18:36:58 +0700

    update domain

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer class="bg-gray-800 shadow">
             <div class="w-full p-4 gap-y-4 py-4">
             <p class="flex justify-center text-sm text-white text-center">
-                Mario Sabda... Made with love💝
+            - Proud to Use 🇮🇩 Domain -
             </p>
             <p class="flex justify-center text-sm text-white text-center">
                 <span className="font-semibold">Copyright@2024</span>. All rights reserved
