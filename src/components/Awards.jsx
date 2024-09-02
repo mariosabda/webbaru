c62e997f30ba7552f2da125e88cc4cb972824ffa
mariosabda <mariosabda@gmail.com>
2024-09-02 10:55:42 +0700

    .

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Awards = () => {
     return (
         <div className="bg-gray-200" id="awards">
-            <h1 className="text-center md:text-4xl sm:text-2xl text-xl font-semibold">Trophy & Awards</h1>
-            <div className="flex flex-row justify-center">
+            <h1 className="text-center md:text-4xl sm:text-2xl text-xl font-semibold pt-4">Trophy & Awards</h1>
+            <div className="flex flex-col justify-center">
                 <a href="https://www.instagram.com/p/Cj-ozEnyc10/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" 
-                className="font-medium mx-auto my-6 py-3 transition transform hover:scale-110 hover:underline hover:text-[#FF204E] duration-300 text-center">
+                className="font-medium pl-10 my-6 py-3 transition transform hover:scale-300 hover:underline hover:text-[#FF204E] duration-300">
                     |FTBI|
                 </a>
                 <a href="https://www.instagram.com/p/Cgdhu3ArStj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" 
-                className="font-medium mx-auto my-6 py-3 transition transform hover:scale-110 hover:underline hover:text-[#FF204E] duration-300 text-center">
+                className="font-medium pl-10 my-6 py-3 transition transform hover:scale-300 hover:underline hover:text-[#FF204E] duration-300">
                     |FLS2N|
                 </a>
                 <a href="https://youtu.be/Txw5Ba2R7n4?si=dEuRzKPBSKV_yMEu" 
-                className="font-medium mx-auto my-6 py-3 transition transform hover:scale-110 hover:underline hover:text-[#FF204E] duration-300 text-center">
+                className="font-medium pl-10 my-6 py-3 transition transform hover:scale-300 hover:underline hover:text-[#FF204E] duration-300">
                     |PESPARANI Nasional 1|
                 </a>
             </div>
