c718421c5e5228acf25d2b42c674c5aafb2088e7
mariosabda <mariosabda@gmail.com>
2024-09-02 22:29:32 +0700

    .

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -5,14 +5,17 @@ const Awards = () => {
         <div className="bg-black text-white" id="awards">
             <h1 className="text-center md:text-4xl sm:text-2xl text-xl font-normal pt-4">Trophy & Awards</h1>
             <div className="flex flex-col justify-center">
-                <span className="font-light pl-10 my-6 py-3">
-                [National] <a className="hover:underline text-[#FF204E] duration-300" href="https://youtu.be/Txw5Ba2R7n4?si=dEuRzKPBSKV_yMEu" >Golden Medalist at Pesta Paduan Suara Gerejani Nasional 1(PESPARANINAS 1)</a> Gregorian Teenagers Choir
+                <span className="font-light pl-10 my-5">
+                [National] <a className="hover:underline text-[#FF204E] duration-300" href="https://youtu.be/Txw5Ba2R7n4?si=dEuRzKPBSKV_yMEu" >Golden Medalist at Pesta Paduan Suara Gerejani Nasional 1 (PESPARANINAS 1)</a>; Children and Youth Gregorian Choir category
                 </span>
-                <span className="font-light pl-10 my-6 py-3">
-                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cj-ozEnyc10/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >1st Place at Festival Bulan Bahasa (FTBI)</a> in Macapat Competition (male category)
+                <span className="font-light pl-10 my-5">
+                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cj-ozEnyc10/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >1st Place at Festival Bulan Bahasa (FTBI)</a>; in Macapat Competition (male category)
                 </span>
-                <span className="font-light pl-10 my-6 py-3">
-                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cgdhu3ArStj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >Festival dan Lomba Seni Siswa Nasional (FLS2N)</a> Solo Vocal
+                <span className="font-light pl-10 my-5">
+                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cgdhu3ArStj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >2nd place at Festival dan Lomba Seni Siswa Nasional (FLS2N)</a>; Solo Vocal category
+                </span>
+                <span className="font-light pl-10 my-5">
+                [Subdistrict] <a className="hover:underline text-[#FF204E] duration-300" href="https://osn-onmipa-bpti.kemdikbud.go.id/osn.html" >1st place at Olimpiade Sains Nasional (OSN)</a>; Mathematics 2019
                 </span>
             </div>
         </div>
