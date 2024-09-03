fb86613d875deaf1e04ab0a560e59d8c02556cda
mariosabda <mariosabda@gmail.com>
2024-09-03 07:52:04 +0700

    .

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -6,13 +6,16 @@ const Awards = () => {
             <h1 className="text-center md:text-4xl sm:text-2xl text-xl font-normal pt-4">Trophy & Awards</h1>
             <div className="flex flex-col justify-center">
                 <span className="font-light pl-10 my-5">
-                [National] <a className="hover:underline text-[#FF204E] duration-300" href="https://youtu.be/Txw5Ba2R7n4?si=dEuRzKPBSKV_yMEu" >Golden Medalist at Pesta Paduan Suara Gerejani Nasional 1 (PESPARANINAS 1)</a>; Children and Youth Gregorian Choir category
+                [National] <a className="hover:underline text-[#FF204E] duration-300" href="https://youtu.be/Txw5Ba2R7n4?si=dEuRzKPBSKV_yMEu" >Golden Medalist at Pesta Paduan Suara Gerejani Nasional 1 (PESPARANI NASIONAL 1)</a>; Children and Youth Gregorian Choir category
                 </span>
                 <span className="font-light pl-10 my-5">
                 [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cj-ozEnyc10/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >1st Place at Festival Bulan Bahasa (FTBI)</a>; in Macapat Competition (male category)
                 </span>
                 <span className="font-light pl-10 my-5">
-                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.instagram.com/p/Cgdhu3ArStj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" >2nd place at Festival dan Lomba Seni Siswa Nasional (FLS2N)</a>; Solo Vocal category
+                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://sma.pusatprestasinasional.kemdikbud.go.id/fls2n/login" >2nd Place at Festival Lomba Seni Siswa Nasional (FLS2N)</a>; in Solo Vocal
+                </span>
+                <span className="font-light pl-10 my-5">
+                [District] <a className="hover:underline text-[#FF204E] duration-300" href="https://www.youtube.com/watch?v=EOsGJg4kDPQ" >3rd Runner-up at English Vlog Competition (FLS2N)</a>; MGMP Bahasa Inggris 2021
                 </span>
                 <span className="font-light pl-10 my-5">
                 [Subdistrict] <a className="hover:underline text-[#FF204E] duration-300" href="https://osn-onmipa-bpti.kemdikbud.go.id/osn.html" >1st place at Olimpiade Sains Nasional (OSN)</a>; Mathematics 2019
